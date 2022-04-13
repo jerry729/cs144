6d6291865b2207b8a71fb1a21561de2319399a2b
Zhaorui Yang <chaojui.yang@outlook.com>
2022-04-14 00:23:54 +0800

    add member variables and finish method ackno() and window_size()

--- libsponge/tcp_receiver.hh
+++ libsponge/tcp_receiver.hh
@@ -19,13 +19,19 @@ class TCPReceiver {
 
     //! The maximum number of bytes we'll store.
     size_t _capacity;
+    WrappingInt32 _ack;
+    bool _is_syn;
 
   public:
     //! \brief Construct a TCP receiver
     //!
     //! \param capacity the maximum number of bytes that the receiver will
     //!                 store in its buffers at any give time.
-    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity) {}
+    TCPReceiver(const size_t capacity) 
+    : _reassembler(capacity)
+    , _capacity(capacity)
+    , _ack(0)
+    , _is_syn(false) {}
 
     //! \name Accessors to provide feedback to the remote TCPSender
     //!@{
